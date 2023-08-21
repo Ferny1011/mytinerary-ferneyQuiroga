c083766c44e03412a44f342f6a4ff57cb44646fb
Ferny1011 <fer-ney1011@outlook.com>
2023-08-20 21:48:45 -0300

    cities template

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import CityCard from '../components/CityCard'
 const Cities = () => {
     return (
-        <div>
-            <h1>Cities</h1>
-        </div>
+        <section>
+            <div className="relative text-center">
+                <img className='w-full h-52 object-cover brightness-50' src="/public/heroCities.jpg" alt="" />
+                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    <h1 className='text-[#FFEAD0] text-5xl' style={{ fontFamily: 'Playfair Display' }} >Cities</h1>
+                    <h4 className='text-2xl text-[#F08CAE] italic'>Choose choose your next memory</h4>
+                </div>
+            </div>
+            <div className='flex justify-center my-16 items-center'>
+                <input className='w-2/4 p-2 rounded-lg focus:outline-[#F08CAE]' type="text" placeholder='Search your city...' />
+            </div>
+            <div className='flex flex-wrap justify-center gap-5 mb-16'>
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+            </div>
+        </section>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
 import Hero from '../components/Hero'
-import '../styles/Home.css'
 
 const Home = () => {
   return (
